1edb4f986300374ad5caafb7716c06eedadfb759
fremgang <jonas.thoresen@gmail.com>
2023-10-13 21:00:43 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.html', './src/*.js', './src/*.json', './css/*.css', './*.html', './*.js', './*.json', './*.css'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
